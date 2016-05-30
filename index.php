996e69ef460a9a9d4da2629aa1918bd0c2bc1618
guuse <ekkelenkampguus@gmail.com>
2016-05-30 10:35:38 +0200

    Added from previous repository

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <script type="text/javascript" src="js/fade.js"></script>
+    <title></title>
+</head>
+    <body>
+<a href="test.php">Test</a>
+    </body>
+</html>
