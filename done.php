7f7a5045673bee59800bc6358a69c90b256c92e8
guuse <ekkelenkampguus@gmail.com>
2016-06-01 16:24:05 +0200

    Added an entire interface
    Added tutorial pages and start page. Fully functional buttons

---
+++ done.php
@@ -0,0 +1,27 @@
+<?php
+$score = 4500;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+     <link rel="stylesheet" href="css/styleDone.css" type="text/css">
+    <script type="text/javascript" src="js/main.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<div class="score">
+    <h1>Score</h1>
+    <p><?=$score?></p>
+</div>
+
+<a href="start.html" class="replay"><img src="images/replay%20button.png"></a>
+<a href="menu.html" class="home"><img src="images/home.png"></a>
+<a href="leaderboards.php" class="lb"><img src="images/leaderboards%20button.png"></a>
+<a href="" class="send"><img src="images/sendbutton.png"></a>
+
+</body>
+</html>
