0533bf0f4f1c2411f369d1317373f9a7f275e0d5
guuse <ekkelenkampguus@gmail.com>
2016-06-06 10:13:40 +0200

    All pages updated and funtional

--- send.php
+++ send.php
@@ -7,7 +7,7 @@ $score = 4500;
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
-     <link rel="stylesheet" href="css/styleDone.css" type="text/css">
+    <link rel="stylesheet" href="css/styleSend.css" type="text/css">
     <script type="text/javascript" src="js/main.js"></script>
     <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
 </head>
@@ -16,12 +16,13 @@ $score = 4500;
 <div class="score">
     <h1>Score</h1>
     <p><?=$score?></p>
+    <a href="start.html" class="replay"><img src="images/replay%20button.png"></a>
+    <a href="menu.html" class="home"><img src="images/home.png"></a>
+    <a href="leaderboardsScore.php" class="lb"><img src="images/leaderboards%20button.png"></a>
+
 </div>
 
-<a href="start.html" class="replay"><img src="images/replay%20button.png"></a>
-<a href="menu.html" class="home"><img src="images/home.png"></a>
-<a href="leaderboardsScore.php" class="lb"><img src="images/leaderboards%20button.png"></a>
-<a href="send.php" class="send"><img src="images/sendbutton.png"></a>
+
 
 </body>
 </html>
